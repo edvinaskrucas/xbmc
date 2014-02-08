0816e32de4dc34f0f5a667c7a9ee907366875758
Edvinas Kručas <edv.krucas@gmail.com>
2014-02-08 13:46:13 +0200

    Added function which loads active audio player

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,32 @@
 var app = angular.module("app", ["ngRoute", "restangular"])
 
-    .controller("Player", function($scope, Restangular) {
+    .controller("Player", function($scope, Restangular, $timeout) {
 
         $scope.player = null;
+
+        /**
+         * Function load active audio player.
+         */
+        var loadAudioPlayer = function() {
+            Restangular.all("Player/GetActivePlayers").getList().then(
+                function(response) {
+                    if (response.data.length > 0) {
+                        if (response.data[0].type == "audio") {
+                            $scope.player = response.data[0];
+                        } else {
+                            $scope.player = null;
+                        }
+                        $timeout(loadAudioPlayer, 1000);
+                    }
+                },
+                function(response) {
+                    $scope.player = null;
+                    $timeout(loadAudioPlayer, 1000);
+                }
+            );
+        }
+
+        loadAudioPlayer();
 
     });
 
